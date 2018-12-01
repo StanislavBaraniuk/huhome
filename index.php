7d47b6a876524f7176f5785e652c5b3525a8a40c
stanislaw <stanislavbaraniuk@gmail.com>
2018-12-01 15:11:12 +0200

    site sources will loading

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+     <title>Home</title>
+     <meta charset="utf-8">
+     <meta name="format-detection" content="telephone=no" />
+     <link rel="icon" href="images/favicon.ico">
+     <link rel="shortcut icon" href="images/favicon.ico" />
+     <link rel="stylesheet" href="./site/css/slippry.css?<?echo time();?>">
+     <link rel="stylesheet" href="./site/css/elastislide.css?<?echo time();?>" />
+     <link rel="stylesheet" href="./site/css/style.css?<?echo time();?>">
+     <script src="./site/js/jquery.js"></script>
+     <script src="./site/js/jquery-migrate-1.1.1.js"></script>
+     <script src="./site/js/script.js"></script>
+     <script src="./site/js/superfish.js"></script>
+     <script src="./site/js/jquery.equalheights.js"></script>
+     <script src="./site/js/jquery.easing.1.3.js"></script>
+     <script src="./site/js/slippry.js"></script>
+    <script src="./site/js/modernizr.custom.17475.js"></script>
+     <script>
+     $(document).ready(function(){
+        jQuery('#slippry').slippry()       
+     }); 
+    function goToByScroll(id){$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');}
+
+     </script>
+        
+   
+    <!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+         </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+      <script src="./site/js/html5shiv.js"></script>
+      <link rel="stylesheet" media="screen" href="./site/css/ie.css">
+
+
+    <![endif]-->
+     </head>
+     <body class="page1" id="top">
+<!--==============================header=================================-->
+<header>
+  <div class="container_12">
+            <div class="grid_12">
+                      <h1>
+          <a href="">
+            <img src="./site/images/logo.png" alt="Your Happy Family">
+          </a>
+        </h1>  
+             
+    </div>
+  </div> 
+  <div class="clear"></div>
+  <ul id="slippry">
+    <li>
+      <a ><img src="./site/images/slide1.jpg" alt="Найкращий клінінг сервіс"></a>
+    </li>
+    <li>
+      <a ><img src="./site/images/slide2.jpg" alt="Свіжість завжди"></a>
+    </li>
+    <li>
+      <a ><img src="./site/images/slide3.jpg" alt="Тримайте ваш будинок в чистоті"></a>
+    </li>
+      <li>
+          <a><img src="./site/images/slide4.jpg" alt="Чистота і порядок"></a>
+      </li>
+  </ul> 
+  <div class="menu_block">
+    <nav class="horizontal-nav full-width horizontalNav-notprocessed">
+      <ul class="sf-menu">
+       <li class="current"><a href="">Головна</a></li>
+       <li><a href="./site/index-2.html">Послуги</a></li>
+       <li><a href="./site/index-3.html">Ціни</a></li>
+       <li><a href="./site/index-4.html">Контакти</a></li>
+     </ul>
+    </nav>
+    <div class="clear"></div>       
+  </div>
+</header>  
+
+<!--=====================Content======================-->
+<section class="content"><div class="ic">More Website Templates @ TemplateMonster.com - May 19, 2014!</div>
+  <div class="container_12">
+    <div class="grid_12">
+      <h2>ПОСЛУГИ</h2>
+    </div>
+    <div class="grid_3"><a href="#" class="box">
+        <div class="maxheight">
+            <a href="./site/index-2.html">
+          <img src="./site/images/page1_img1.jpg" alt="">
+          <div class="box_bot">
+            КУХНІ
+            <span>Прибирання</span>
+          </div>
+            </a>
+        </div>
+      </a></div>
+    <div class="grid_3"><a href="#" class="box">
+        <div class="maxheight">
+            <a href="./site/index-2.html">
+          <img src="./site/images/page1_img2.jpg" alt="">
+          <div class="box_bot">
+            ОФІСИ
+            <span>Прибирання</span>
+          </div>
+            </a>
+        </div>
+      </a></div>
+    <div class="grid_3"><a href="#" class="box">
+        <div class="maxheight">
+            <a href="./site/index-2.html">
+          <img src="./site/images/page1_img3.jpg" alt="">
+          <div class="box_bot">
+            ВІКНА
+            <span>Чистка</span>
+          </div>
+            </a>
+        </div>
+      </a></div>
+    <div class="grid_3"><a href="#" class="box">
+        <div class="maxheight">
+            <a href="./site/index-2.html">
+          <img src="./site/images/page1_img4.jpg" alt="">
+          <div class="box_bot">
+            Переглянути
+            <span>всі послуги</span>
+          </div>
+            </a>
+        </div>
+      </a></div>
+    <div class="grid_12 center">
+        <h2>Гарячі пропозиції</h2>
+        <div class="text1">-20% за промокодом</div>
+        <img src="./site/images/banner.jpg" class="ban_img" alt="">
+    </div>
+      <div class="grid_12 center">
+      <a href="#" ></a>
+    </div>
+  </div>    
+  <div class="clear"></div>
+  <div class="sep__1"></div>
+  <div class="container_12">
+    <div class="grid_6 prefix_3" style="text-align: center">
+      <h3>Про нас</h3>
+        <p>Найкраща клінінгова компанія міста. <br><br>НадаbrН майже всі можливі види послуг. <br><br>Швидко та якісно.</p>
+    </div>
+
+    <div class="clear"></div>
+    <div class="grid_12">
+      <h2 class="head1">Галерея</h2>
+    </div>
+  </div>
+  <div class="clear"></div>
+  <!-- Elastislide Carousel -->
+  <ul id="carousel" class="elastislide-list">
+        <li><a href="#"><img src="./site/images/gall_img1.jpg" alt="image01" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img2.jpg" alt="image02" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img3.jpg" alt="image03" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img4.jpg" alt="image04" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img5.jpg" alt="image05" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img6.jpg" alt="image06" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img7.jpg" alt="image07" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img8.jpg" alt="image08" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img9.jpg" alt="image01" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img10.jpg" alt="image02" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img11.jpg" alt="image03" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img12.jpg" alt="image04" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img13.jpg" alt="image05" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img14.jpg" alt="image06" /></a></li>
+        <li><a href="#"><img src="./site/images/gall_img15.jpg" alt="image07" /></a></li>
+  </ul>
+  <!-- End Elastislide Carousel -->
+</section>
+<!--==============================Bot_block=================================-->
+<!--==============================footer=================================-->
+<footer> 
+  <div class="container_12">
+    <div class="row">
+      <div class="grid_12">  
+            <a href="#" onclick="goToByScroll('top'); return false;" class="top"></a><br> <a href="#" class="f_logo"><img src="./site/images/f_logo.png" alt=""></a>
+          <div class="footer_socials">
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-google-plus"></a>
+              <a href="#" class="fa fa-instagram"></a>
+          </div>
+      </div>
+    </div>
+  </div>  
+  <div class="clear"></div>
+</footer>
+<script type="text/javascript" src="./site/js/jquerypp.custom.js"></script>
+    <script type="text/javascript" src="./site/js/jquery.elastislide.js"></script>
+    <script type="text/javascript">
+      
+      $( '#carousel' ).elastislide( {
+        minItems : 8
+      } );
+      
+    </script>
+</body>
+</html>
+
---
+++ site/index-3.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+     <title>Ціни</title>
+     <meta charset="utf-8">
+     <meta name="format-detection" content="telephone=no" />
+     <link rel="icon" href="images/favicon.ico">
+     <link rel="shortcut icon" href="images/favicon.ico" />
+     <link rel="stylesheet" href="css/style.css?<?echo time();?>">
+     <script src="js/jquery.js"></script>
+     <script src="js/jquery-migrate-1.1.1.js"></script>
+     <script src="js/script.js"></script> 
+     <script src="js/superfish.js"></script>
+     <script src="js/jquery.equalheights.js"></script>
+     <script src="js/jquery.easing.1.3.js"></script>
+     <script>
+
+    function goToByScroll(id){$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');}
+
+     </script>
+        
+   
+    <!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+         </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <link rel="stylesheet" media="screen" href="css/ie.css">
+
+
+    <![endif]-->
+     </head>
+     <body class="" id="top">
+<!--==============================header=================================-->
+<header>
+  <div class="container_12">
+            <div class="grid_12">
+                      <h1>
+          <a href="../index.php">
+            <img src="images/logo.png" alt="Your Happy Family">
+          </a>
+        </h1>  
+             
+    </div>
+  </div> 
+  <div class="clear"></div>
+  <div class="menu_block">
+    <nav class="horizontal-nav full-width horizontalNav-notprocessed">
+      <ul class="sf-menu">
+       <li><a href="../index.php">Головна</a></li>
+       <li><a href="index-2.html">Послуги</a></li>
+       <li class="current"><a href="index-3.php">Ціни</a></li>
+       <li><a href="index-4.html">Контакти</a></li>
+     </ul>
+    </nav>
+    <div class="clear"></div>       
+  </div>
+</header>  
+
+<!--=====================Content======================-->
+<section class="content">
+  <div class="container_12">
+    <div class="grid_8">
+      <h4 class="head1">Список цін</h4>
+      <div class="grid_4 alpha">
+        <p class="color1">Будинки </p>
+        <ul class="list2">
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Мийка вікон (вікно)</a></div>
+            <div class="price">50 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Мийка полу (кімната)</a></div>
+            <div class="price">100 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Прибирання пилу з освітлювальних приладів</a></div>
+            <div class="price">50 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Сухе прибирання полів пилососом. (кімната)</a></div>
+            <div class="price">100 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Винос сміття (до 5кг)</a></div>
+            <div class="price">50 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Стірка</a></div>
+            <div class="price">150 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Прибирання домашніх речей</a></div>
+            <div class="price">100 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Прибирання пилу з розеток і вимикачів</a></div>
+            <div class="price">50 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Генеральне прибирання санвузла</a></div>
+            <div class="price">250грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Протирання дзеркал і скляних поверхонь</a></div>
+            <div class="price">150 грн</div>
+          </li>
+        </ul>
+      </div>
+      <div class="grid_4 omega">
+        <p class="color1">Офіси</p>
+        <ul class="list2">
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Щоденне прибирання</a></div>
+            <div class="price"></div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">Разове прибирання офісів</a></div>
+            <div class="price"></div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">30-70(м2)</a></div>
+            <div class="price">1400 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">70-110(м2)</a></div>
+            <div class="price">2200 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">110-150(м2)</a></div>
+            <div class="price">3000 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">110-150(м2)</a></div>
+            <div class="price">4000 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">200-250(м2)</a></div>
+            <div class="price">5000грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">250-300(м2)</a></div>
+            <div class="price">6000 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">300-400(м2)</a></div>
+            <div class="price">7600 грн</div>
+          </li>
+          <li>
+            <div class="prod"><a onclick="openOrderFrame()">від 400(м2)</a></div>
+            <div class="price">Договірна</div>
+          </li>
+        </ul>
+      </div>
+      <div class="clear"></div>
+    </div>
+    <div class="grid_4">
+      <h4 class="head2">Чому ми?</h4>
+      <p class="color1"><a onclick="openOrderFrame()">Зручнсіть  </a></p>
+      <p class="p1">Прибиремо будь яке приміщення швидко та якісно</p>
+      <p class="color1"><a onclick="openOrderFrame()">Підтримка </a></p>
+      <p class="p1">Підтримка для клієнтів працює 24/7</p>
+      <p class="color1"><a onclick="openOrderFrame()">Комфорт  </a></p>
+      Прибирання всії приміщень одним дзвінком <br>
+
+    </div>
+    <div class="clear"></div>
+  </div>
+  <div class="sep__2"></div>
+  <div class="container_12">
+    <div class="grid_8">
+      <h3>Додаткові</h3>
+      <ul class="list2">
+        <li>
+          <div class="prod"><a onclick="openOrderFrame()">Хімистка</a></div>
+          <div class="price"></div>
+        </li>
+        <li>
+          <div class="prod"><a onclick="openOrderFrame()" style="padding-left: 50px">Ковролін</a></div>
+          <div class="price">від 10 грн</div>
+        </li>
+        <li>
+          <div class="prod"><a onclick="openOrderFrame()" style="padding-left: 50px">Дивани</a></div>
+          <div class="price">60 грн</div>
+        </li>
+        <li>
+          <div class="prod"><a onclick="openOrderFrame()" style="padding-left: 50px">Матраси</a></div>
+          <div class="price">від 150 грн</div>
+        </li>
+        <li>
+          <div class="prod"><a onclick="openOrderFrame()" style="padding-left: 50px">Офісні крісла</a></div>
+          <div class="price">від 17 грн</div>
+        </li>
+        <li>
+          <div class="prod"><a onclick="openOrderFrame()" style="padding-left: 50px">Килим</a></div>
+          <div class="price">від 20 грн</div>
+        </li>
+        <li>
+          <div class="prod"><a onclick="openOrderFrame()">Мийка автомобілів</a></div>
+          <div class="price">500 грн</div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="grid_4">
+      <h3>Скидки</h3>
+      <p class="color1"><a>Промокод </a></p>
+      <p>За кожне використання наших послуг видаэться 5% промокод на наступне прибиирання</p>
+    </div>
+    <div class="clear"></div>
+  </div>
+</section>
+<!--==============================Bot_block=================================-->
+<!--==============================footer=================================-->
+<footer> 
+  <div class="container_12">
+    <div class="row">
+      <div class="grid_12">  
+            <a onclick="openOrderFrame()" onclick="goToByScroll('top'); return false;" class="top"></a><br> <a onclick="openOrderFrame()" class="f_logo"><img src="images/f_logo.png" alt=""></a>
+          <div class="footer_socials">
+            <a onclick="openOrderFrame()" class="fa fa-twitter"></a>
+            <a onclick="openOrderFrame()" class="fa fa-facebook"></a>
+            <a onclick="openOrderFrame()" class="fa fa-google-plus"></a>
+            <a onclick="openOrderFrame()" class="fa fa-instagram"></a>
+          </div>
+      </div>
+    </div>
+  </div>  
+  <div class="clear"></div>
+</footer>
+
+
+<script>
+  function openOrderFrame() {
+      alert("d");
+  }
+</script>
+
+</body>
+</html>
+
